6c24a314f764585cabf11a15becde1eae0a2967d
funny123 <524656859@qq.com>
2021-03-05 21:39:00 +0800

    菜谱接口，接入unipush推送

--- app/Controller/Auth/LoginController.php
+++ app/Controller/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
     {
 //         $hash = password_hash($this->request->input('password'), PASSWORD_DEFAULT);
 //         return $this->failed($hash);
-        $user = User::query()->where('account', $this->request->input('account'))->first();
+        $user = User::query()->where('account', $this->request->input('username'))->first();
         //验证用户账户密码
         if  (!empty($user->password) && password_verify($this->request->input('password'), $user->password))  {
             $userData = [
